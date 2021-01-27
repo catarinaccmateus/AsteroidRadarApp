d40cbf687352046245ab6867b825f73fae88ed64
nb26548 <nb26548@novabase.pt>
2021-01-27 15:34:36 +0000

    Setting up project & Creating layout for asteroid list item.

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,18 @@
 package com.udacity.asteroidradar.main
 
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.Asteroid
+
+/* For testing purposes */
+val listOfAsteroids: List<Asteroid> = listOf(
+    Asteroid(2939L, "asteroid1", "2020-11-02", 9399.9, 929202.2, 20202.2, 2020.22, true),
+    Asteroid(2939L, "asteroid1", "2020-11-02", 9399.9, 929202.2, 20202.2, 2020.22, true),
+    Asteroid(2939L, "asteroid1", "2020-11-02", 9399.9, 929202.2, 20202.2, 2020.22, true),
+    Asteroid(2939L, "asteroid1", "2020-11-02", 9399.9, 929202.2, 20202.2, 2020.22, true),
+    Asteroid(2939L, "asteroid1", "2020-11-02", 9399.9, 929202.2, 20202.2, 2020.22, true)
+    )
+
 
 class MainViewModel : ViewModel() {
+    val listOfAsteroids: List<Asteroid> = listOf(Asteroid(2939L, "asteroid1", "2020-11-02", 9399.9, 929202.2, 20202.2, 2020.22, true))
 }
