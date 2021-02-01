8c14ff8f13e1f3cdcb9ec900154bbe473dfcd792
nb26548 <nb26548@novabase.pt>
2021-02-01 01:45:40 +0000

    Adding extra comments

--- starter/app/src/main/java/com/udacity/asteroidradar/utils/Utils.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/utils/Utils.kt
@@ -18,5 +18,7 @@ fun getLastWeekDayString(): String {
     val leftDays = Calendar.SUNDAY - currentDay
     calendar.add(Calendar.DATE, leftDays)
     val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+    /** Help - when filtering it doesnt show any asteroids for the current week, even though it exists.
+     *  Tried to log this date unsuccessfully */
     return dateFormat.format(calendar.time)
 }
